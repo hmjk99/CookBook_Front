86b93ee2895579e8227d10897ab046396b2777ec
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-04-12 01:04:35 -0400

    edited axios and got recipes page to work

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import axios from "axios";
 import Recipes from "./components/recipes";
 import Add from "./components/add";
-import Edit from "./components/edit";
+// import Edit from "./components/edit";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  const [users, setUsers] = useState([]);
 
   // need the url from back end
   const getRecipes = () => {
-    axios.get("http://localhost:8000/api/").then(
+    axios.get("http://localhost:8000/api/recipes").then(
       (response) => setRecipes(response.data),
       (err) => console.log(err)
     );
   };
 
-  useEffect(() => {
-    getRecipes();
-  }, []);
+  const getUsers = () => {
+    axios.get("http://localhost:8000/api/userprofiles").then(
+      (response) => setUsers(response.data),
+      (err) => console.log(err)
+    );
+  };
 
   const handleCreate = (addRecipe) => {
-    axios.post("http://localhost:8000/api/", arrRecipe).then((response) => {
+    axios.post("http://localhost:8000/api/recipes", addRecipe).then((response) => {
       console.log(response);
       getRecipes();
     });
@@ -31,7 +35,7 @@ function App() {
   const handleUpdate = (editRecipe) => {
     console.log(editRecipe);
     axios
-      .put("http://localhost:8000/api/" + editRecipe.id, editRecipe)
+      .put("http://localhost:8000/api/recipes/" + editRecipe.id, editRecipe)
       .then((response) => {
         getRecipes();
       });
@@ -39,26 +43,29 @@ function App() {
 
   const handleDelete = (e) => {
     axios
-      .delete("http://localhost:8000/api/" + e.target.value)
+      .delete("http://localhost:8000/api/recipes/" + e.target.value)
       .then((response) => {
         getRecipes();
       });
   };
 
+  useEffect(() => {
+    getRecipes()
+    getUsers()
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<Recipes recipes={recipes} handleDelete={handleDelete} />}
-        />
-        <Route path="/add" element={<Add handleCreate={handleCreate} />} />
-        <Route
-          path="/edit/:id"
-          element={<Edit recipes={recipes} handleEdit={handleEdit} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={<Recipes recipes={recipes} handleDelete={handleDelete} />}
+      />
+      <Route path="/add" element={<Add handleCreate={handleCreate} />} />
+      {/* <Route
+        path="/edit/:id"
+        element={<Edit recipes={recipes} handleEdit={handleEdit} />}
+      /> */}
+    </Routes>
   );
 }
 
--- src/components/recipes.js
+++ src/components/recipes.js
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import {Link} from 'react-router-dom'
+
 
 const Recipes = (props) => {
   return (
     <>
       <div>
-        {recipes.map((recipe) => {
-          <ul>
-            <li></li>
-          </ul>;
+        <h1>Recipes</h1>
+        <Link to='/add'><button>Add New Recipe</button></Link>
+        {props.recipes.map((recipe) => {
+          return (
+            <div>
+              <h4>{recipe.title}</h4>
+              <img src={recipe.image}/>
+              <h4>Equipment: {recipe.equipment}</h4>
+              <h4>Ingredients: {recipe.ingredients}</h4>      
+              <h4>Instructions: {recipe.instructions}</h4>     
+            </div>
+          )
         })}
       </div>
     </>
