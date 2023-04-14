50cc6eec71e569edc4782c2add5dd7696bee4ec0
Steven Gebhardt <stevengebhardt@stevens-mbp.lan>
2023-04-14 14:52:51 -0400

    search bar completed

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Recipes = (props) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <>
       <div>
@@ -8,19 +11,45 @@ const Recipes = (props) => {
         <Link to="/add">
           <button>Add New Recipe</button>
         </Link>
-        {props.recipes.map((recipe) => {
-          return (
-            <div>
-              <Link to={`/${recipe.id}`}>
-                <h4>{recipe.title}</h4>
-                <img src={recipe.image} />
-              </Link>
-              <button onClick={props.handleDelete} value={recipe.id}>
-                X
-              </button>
-            </div>
-          );
-        })}
+        <div>
+          <input
+            type="search"
+            placeholder="search by recipe name..."
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+        </div>
+        {props.recipes
+          .filter((recipe) => {
+            if (searchTerm == "") {
+              return recipe;
+            } else if (
+              recipe.title
+                .toLowerCase()
+                .includes(searchTerm.toLocaleLowerCase())
+            ) {
+              return (
+                <Link to={`/${recipe.id}`}>
+                  <h4>{recipe.title}</h4>
+                  <img src={recipe.image} />
+                </Link>
+              );
+            }
+          })
+          .map((recipe) => {
+            return (
+              <div>
+                <Link to={`/${recipe.id}`}>
+                  <h4>{recipe.title}</h4>
+                  <img src={recipe.image} />
+                </Link>
+                <button onClick={props.handleDelete} value={recipe.id}>
+                  X
+                </button>
+              </div>
+            );
+          })}
       </div>
     </>
   );
